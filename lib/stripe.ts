5aa3ed1db1f8a14a2e545e7970e1e91a04be50a2
bambafall1902-star <bambafall1902@gmail.com>
2026-04-22 23:30:06 +0000

    fix: stripe version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16'
+  apiVersion: '2026-03-25.basil' as any
 })
 
 export const STRIPE_PRICE_ID = process.env.STRIPE_PRICE_ID!
