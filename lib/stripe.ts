56234b43a596dea29e5ab009745ef04e7f82c5fc
bambafall1902-star <bambafall1902@gmail.com>
2026-04-23 04:50:27 +0000

    fix: remove stripe api version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,5 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-03-25.basil' as any
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export const STRIPE_PRICE_ID = process.env.STRIPE_PRICE_ID!
